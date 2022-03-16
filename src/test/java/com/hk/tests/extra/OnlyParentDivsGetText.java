1d38590d8fd02ffe7f0648d695d73ae40849395c
HakanK003 <hkaratashk3@gmail.com>
2022-03-15 22:35:55 -0400

    gitignore update 2 and new method for texts

---
+++ src/test/java/com/hk/tests/extra/OnlyParentDivsGetText.java
@@ -0,0 +1,12 @@
+package com.hk.tests.extra;
+
+public class OnlyParentDivsGetText {
+
+    /*
+
+            WebElement onlyParentsText = Jsoup.parse(each.getAttribute("outerHTML")).selectFirst("cssSelectorLocator").ownText());
+
+     */
+
+
+}
